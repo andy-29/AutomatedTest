5418439ffb0064b869b9e1fe16248d12cabcafea
Administrator <dayuezaichunji@163.com>
2019-02-20 12:00:32 +0800

    生产环境调整

--- testCase/sign_check_in_test.py
+++ testCase/sign_check_in_test.py
@@ -1,20 +1,11 @@
-import gmhttp
-import unittest
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-import sys,hashlib,datetime
-sys.path.append(BASE_DIR)
-from common.get_config import g
+import os, sys
 
-
-
-
-
-import os,sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 func = os.path.basename(__file__).split('_test.py')[0]
 from common.gmpackage import *
+
+
 @ddt
 class Sign_Check_In(unittest.TestCase):
     '''
@@ -26,50 +17,18 @@ class Sign_Check_In(unittest.TestCase):
         cls.host = g.host
         cls.api_name = g.api_name(func)
         cls.url = cls.host + cls.api_name
-        print('获取环境信息和接口信息')
-        self.host = g.host
-        self.api_name = g.api_name(os.path.basename(__file__).split('_test.py')[0])
-        self.android_params = g.android_params
+        cls.activity_id, _, cls.sign_status, cls.total_days = sign_activity_id_get()
 
-        self.user_telephone = g.get_info('user_info', 'telephone')
-        self.password = g.get_info('user_info', 'password')
-        from common.getcookie import create_header
-        self.header = create_header(self.user_telephone, self.password, 'android_params').get('header')
-        self.assertTrue(self.header, msg='登录未成功！')
     @data(*(get_values(func, "test_sign_check_in")))
-    def test_sign_check_in(self,value):
-        self._testMethodDoc = "--"
-        '''
-        
-        '''
-
-        signdata = hashlib.md5((g.get_info('env_info','sign_salt')+str(datetime.date.today())).encode()).hexdigest()
-
-        url = self.host + self.api_name+ "?" + self.android_params
-        r = gmhttp.post(url,verify=False,headers=self.header,data={'sign_date':signdata})
-        self.assertEqual(r.status_code,200,'返回码不为200！')
-        r = r.json()
-        self.assertEqual(r.get("error"),0)
-        self.assertIn('data', r.keys())
-        self.assertIn('status', r.get('data').keys())
-        self.assertIn(r.get('data').get('status'), (1,2,3))  #脚本一天执行次数较多，该处不做等值判断
-        self.assertIn('float_window', r.get('data').keys())
-        self.assertIsInstance(r.get('data').get('float_window'), bool)
-        self.assertIn('time', r.get('data').keys())
-        self.assertIsInstance(r.get('data').get('time'), str)
-        self.assertIn('msg', r.get('data').keys())
-        self.assertIsInstance(r.get('data').get('msg'), str)
-        self.assertIn('sign_status', r.get('data').keys())
-        self.assertTrue(r.get('data').get('sign_status'))
-
-        '''每天只能签到一次，再次签到'''
-        signdata = hashlib.md5((g.get_info('env_info','sign_salt')+str(datetime.date.today())).encode()).hexdigest()
-        url = self.host + self.api_name+ "?" + self.android_params
-        r = gmhttp.post(url,verify=False,headers=self.header,data={'sign_date':signdata})
-        r=r.json()
-        self.assertEqual(r.get('data').get('status'), 2)
-        print('用例执行完毕!')
-
+    @require_login
+    def test_sign_check_in(self, value):
+        self._testMethodDoc = "签到"
+        data = {
+            'sign_type': 2 if self.sign_status == 2 else 1,
+            'activity_id': self.activity_id
+        }
+        r = gmhttp.post(self.url, data=data).json()
+        self.assertIsInstance(r.get("error"), int)
 
     def tearDown(self):
         pass
--- testCase/sign_has_guide_test.py
+++ testCase/sign_has_guide_test.py
@@ -1,15 +1,3 @@
-import gmhttp
-import unittest
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-import sys
-sys.path.append(BASE_DIR)
-from common.get_config import g
-
-
-
-
-
 import os,sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -26,27 +14,12 @@ class Sign_Has_Guide(unittest.TestCase):
         cls.host = g.host
         cls.api_name = g.api_name(func)
         cls.url = cls.host + cls.api_name
-        print('获取环境信息和接口信息')
-        self.host = g.host
-        self.api_name = g.api_name(os.path.basename(__file__).split('_test.py')[0])
-        self.android_params = g.android_params
 
-        self.user_telephone = g.get_info('user_info', 'telephone')
-        self.password = g.get_info('user_info', 'password')
-
-        from common.getcookie import create_header
-        self.header = create_header(self.user_telephone, self.password, 'android_params').get('header')
-        self.assertTrue(self.header, msg='登录未成功！')
     @data(*(get_values(func, "test_sign_has_guide")))
+    @require_login
     def test_sign_has_guide(self,value):
-        self._testMethodDoc = "--"
-        '''
-        签到详情
-        '''
-        url = self.host + self.api_name+ "?" + self.android_params
-        r = gmhttp.get(url,verify=False,headers = self.header)
-        self.assertEqual(r.status_code,200,'返回码不为200！')
-        r = r.json()
+        self._testMethodDoc = '获取签到配置'
+        r = gmhttp.get(self.url).json()
         self.assertEqual(r.get("error"),0)
         print('用例执行完毕!')
 
